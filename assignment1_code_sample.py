774608b58f233654f81683ec7189936df32f7052
tanellelavallee <tanellelavallee@gmail.com>
2025-03-18 12:47:16 -0500

    Testing: Given comments based on failed tests

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -13,14 +13,19 @@ def get_user_input():
     return user_input
 
 def send_email(to, subject, body):
+    # Starting a process with a shell, command injection without proper sanitization.
+    # Attacker can control any of the to, subject, body with malicious shell commands (delete system files)
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
 def get_data():
+    # Uses HTTP instead of HTTPS - data not encrypted and can be modified by attackers
     url = 'http://insecure-api.com/get-data'
+    # urlopen can be used to get local files
     data = urlopen(url).read().decode()
     return data
 
 def save_to_db(data):
+    # SQL query allows attackers to manipulate the database, delete, modify
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
